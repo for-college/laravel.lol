5f8066941b86b035c4fc43a6aa39b864a1f08ddb
scffs <khidey@inbox.ru>
2023-11-11 15:39:24 +0700

    change tab size to 2 from 4

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,51 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+/** Создание маршрута /test с выводом сообщения Тестовая страница **/
+
+Route::get('/test', function () {
+  return "Тестовая страница";
+});
+
+Route::get('/test/2', function () {
+  return 'Какое-то сообщение';
+});
+
+/** Параметры маршрута **/
+
+Route::get('/post/{id}', function ($id) {
+  return "Пост $id";
+});
+
+Route::get('/user/{name}', function ($name) {
+  return "Имя $name";
+});
+
+/** Несколько параметров в маршруте **/
+
+Route::get('/post/{cardId}/{postId}', function ($cardId, $postId) {
+  return "$cardId - $postId";
+});
+
+Route::get('/user/{surname}/{name}', function ($surname, $name) {
+  return "Привет, $surname $name";
+});
+
+/** Необязательные параметры **/
+
+Route::get('/posts/{page?}', function ($page = 1) {
+  return "Номер страницы " . $page;
+});
+
+Route::get('/city/{city?}', function ($city = 'Томск') {
+  return "Город " . $city;
+});
+
+/** Ограничение параметров **/
+
+Route::get('/user/{age}', function ($age) {
+  return "Возраст пользователя " . $age;
 });
