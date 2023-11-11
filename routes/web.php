6a401b7a4b2da710ddd2af9b9ea1da851565a42b
scffs <khidey@inbox.ru>
2023-11-11 16:11:01 +0700

    Изучили маршрутизацию

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,37 @@ Route::get('/city/{city?}', function ($city = 'Томск') {
 
 /** Ограничение параметров **/
 
-Route::get('/user/{age}', function ($age) {
-  return "Возраст пользователя " . $age;
+Route::get('/users/{age}', function ($age) {
+  return "Возраст пользователя: " . $age;
+})->where('age', '[0-9]+');
+
+Route::get('/govsign/{sign}/{id}', function ($sign, $id) {
+  return "Номер: " . $sign . ". Регион: " . $id;
+})->where('sign', '[a-z]+');
+
+/**
+ * ===========================================
+ * whereAlpha - только буквы
+ * whereNumber - только цифры
+ * whereAlphaNumeric - только буквы и цифры
+ * ===========================================
+**/
+
+Route::get('/govsign2/{sign}/{id}', function ($sign, $id) {
+  return "Номер: " . $sign . ". Регион: " . $id;
+})->whereAlpha('sign');
+
+/**
+ * Разрешение конфликтов в маршрутах
+ * Сначала указываем частный случай, а потом - общие
+**/
+
+Route::prefix('/test3')->group(function () {
+  Route::get('/all', function () {
+    return "Все тесты";
+  });
+
+  Route::get('/{n}', function ($n) {
+    return "Тест - " . $n;
+  });
 });
